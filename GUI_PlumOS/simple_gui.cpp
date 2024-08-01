7949ee8a865260cd296a97149552aa4c8267a218
JkFussy <dimasurkov13@outlook.com>
2024-08-01 20:44:21 +0300

    Update simple_gui.cpp

--- GUI_PlumOS/simple_gui.cpp
+++ GUI_PlumOS/simple_gui.cpp
@@ -1,51 +1,151 @@
 #include <SDL.h>
+#include <SDL_vulkan.h>
+#include <GL/glew.h>
+#include <vulkan/vulkan.h>
 #include <iostream>
+#include <vector>
 
-const int WINDOW_WIDTH = 1920;
-const int WINDOW_HEIGHT = 1080;
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
 
-bool handleEvents(SDL_Rect& buttonRect, bool& colorToggle) {
-	SDL_Event event;
-	while (SDL_PollEvent(&event)) {
-	    if (event.type == SDL_QUIT) {
-		return false;
-	    } else if (event.type == SDL_MOUSEBUTTONDO
-		int x, y;
-		SDL_GetMouseState(&x, &y);
-		if (x >= buttonRect.x && x <= buttonRect.x + buttonRect.w &&
-		    y >= buttonRect.y && y <= buttonRect.y + buttonRect.h) {
-		    colorToggle = !colorToggle;
-		}
-	    }
-	}
-	return true;
+enum RenderMode {
+    OpenGLMode,
+    VulkanMode
+};
 
+bool handleEvents(RenderMode &mode) {
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            return false;
+        }
+        if (event.type == SDL_KEYDOWN) {
+            if (event.key.keysym.sym == SDLK_SPACE) {
+                mode = (mode == OpenGLMode) ? VulkanMode : OpenGLMode;
+            }
+        }
+    }
+    return true;
 }
 
-void render(SDL_Renderer* renderer, SDL_Rect& squareRect, bool colorToggle) {
+void renderOpenGL() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-	SDL_RenderClear(renderer);
-	
-	if (colorToggle) {
-		SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-	} else {
-		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-	}
-	SDL_RenderFillRect(renderer, &squareRect);
+    // Render a simple rectangle
+    glBegin(GL_QUADS);
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glVertex2f(-0.5f, -0.5f);
+    glVertex2f( 0.5f, -0.5f);
+    glVertex2f( 0.5f,  0.5f);
+    glVertex2f(-0.5f,  0.5f);
+    glEnd();
+}
 
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
-	SDL_RenderFillRect(renderer, &buttonRect);
-
-	SDL_RenderPresent(renderer);
+void renderVulkan(VkInstance instance, VkSurfaceKHR surface) {
+    // Here should be Vulkan rendering code
+    // For simplicity, it's not implemented in this example
+    std::cout << "Rendering with Vulkan (not implemented in this example)" << std::endl;
 }
 
 int main(int argc, char* argv[]) {
-	if (SDL_Init(SDL_INIT_VIDEO)
-		std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-		return 1;
-	}
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
 
-	SDL_Window* window = SDL_CreateWindow("Simple SDL2 GUI", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
-	if (window == nullptr) {
-		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+    SDL_Window* window = SDL_CreateWindow("OpenGL and Vulkan Example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_VULKAN);
+    if (window == nullptr) {
+        std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        SDL_Quit();
+        return 1;
+    }
+
+    // OpenGL setup
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+    if (glContext == nullptr) {
+        std::cerr << "SDL_GL_CreateContext Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK) {
+        std::cerr << "glewInit Error" << std::endl;
+        SDL_GL_DeleteContext(glContext);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    // Vulkan setup
+    VkInstance instance;
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Hello Vulkan";
+    appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.pEngineName = "No Engine";
+    appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    VkInstanceCreateInfo createInfo = {};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    unsigned int extensionCount = 0;
+    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, nullptr)) {
+        std::cerr << "SDL_Vulkan_GetInstanceExtensions Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    std::vector<const char*> extensions(extensionCount);
+    if (!SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions.data())) {
+        std::cerr << "SDL_Vulkan_GetInstanceExtensions Error: " << SDL_GetError() << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+    createInfo.ppEnabledExtensionNames = extensions.data();
+
+    if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+        std::cerr << "Failed to create Vulkan instance" << std::endl;
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    VkSurfaceKHR surface;
+    if (!SDL_Vulkan_CreateSurface(window, instance, &surface)) {
+        std::cerr << "SDL_Vulkan_CreateSurface Error: " << SDL_GetError() << std::endl;
+        vkDestroyInstance(instance, nullptr);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    RenderMode mode = OpenGLMode;
+    bool running = true;
+    while (running) {
+        running = handleEvents(mode);
+        if (mode == OpenGLMode) {
+            SDL_GL_MakeCurrent(window, glContext);
+            renderOpenGL();
+            SDL_GL_SwapWindow(window);
+        } else {
+            renderVulkan(instance, surface);
+        }
+    }
+
+    vkDestroySurfaceKHR(instance, surface);
+    vkDestroyInstance(instance, nullptr);
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
